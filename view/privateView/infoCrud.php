4c98ed5fe0a9bc40724801b2d56b1fc0e72b846f
JonRauzy <jonathan.rauzy@gmail.com>
2023-09-18 12:13:41 +0200

    bouton delete réparé

--- view/privateView/infoCrud.php
+++ view/privateView/infoCrud.php
@@ -55,7 +55,7 @@ include_once "../view/include/header.php";
                     </button>
                 </td>
                 <td>
-                    <button class="btn">
+                    <button>
                         <a onclick="void(0);let a=confirm('Voulez-vous vraiment supprimer \'<?= $info->getMwIdInfo() ?>\' ?'); if(a){ document.location = '?p=info&info-delete=<?= $info->getMwIdInfo() ?>'; };" href="#">delete</a>
                     </button>
                 </td>
