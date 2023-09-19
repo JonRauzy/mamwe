2bdc27814d3a95be60da343cb052995b478713ca
JonRauzy <jonathan.rauzy@gmail.com>
2023-09-19 07:59:20 +0200

    test nav v1e

--- view/include/header.php
+++ view/include/header.php
@@ -12,6 +12,10 @@
     <link rel="stylesheet" href="css/livredor.css">    
     <link rel="stylesheet" href="css/ressources.css">
     <link rel="stylesheet" href="css/crud.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;700&family=Metrophobic&family=Roboto&family=Waiting+for+the+Sunrise&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="js/tinymce.js/tinymce/tinymce.min.js"></script>
     <script>
@@ -33,46 +37,47 @@
 
 <!-- Affichage de la barre de navigation pour le public -->
 <div class="public-nav">
-    <div class="titre" ><img  src="asset/img/titre.png"></div>
-            <nav>  
-                <img class="logo" src="asset/img/logo1.png">
-                <a href="./">Accueil</a>
-            
-                <!-- lien pour les category :-->
-                <?php
-                $allSection = $sectionManager -> getAll();
-                
-                foreach($allSection as $section) :  
-                ?>
-                <!-- VERIFIER DB pour les noms -->
-                <div class="menu_ressouces">
-                    <a class="menu" href="?sectionId=<?= $section -> getMwIdSect() ?>"><?= $section -> getMwTitleSect() ;?></a>
-             <!--       <menu id="ressource-select">
-                        <?php foreach($articleBySection as $abs):?>
-                                <li> <a href="#<?=$abs->getmwTitleArt()?>">
-                                    <?=$abs->getmwTitleArt()?></a>
-                                </li>
-                        <?php endforeach; ?>
-                    </menu>  -->
-                </div>
-                <?php
-                endforeach;
-                ?> 
-                <div class="menu_ressouces">
-                    <a href="?p=ressources">Ressources</a>
-                    <!----sous-menu ressources   ----->
-                    <menu id="ressource-select">     
-                        <?php foreach($allCategory as $category):?>
-                            <li> <a href="#<?=$category->getMwTitleCategory()?>">
-                                <?=$category->getMwTitleCategory()?></a>
-                            </li>
-                        <?php endforeach; ?>
-                    </menu>
-                </div>
-                <a href="?p=agenda">Agenda</a>
-                <a href="?p=contact">Contact</a>
-                <a href="?p=livreDor">Livre D'or</a>
-            </nav>
+    <div class="titre">
+        <h1>MAMWE</h1>
+        <h2>Sage-femme de Bruxelles</h2>
     </div>
+    <img class="logo" src="asset/img/logo1.png">
+    <nav>  
+        <a href="./">Accueil</a>
+
+        <!-- lien pour les category :-->
+        <?php                
+        foreach($allSection as $section) :  
+        ?>
+        <!-- VERIFIER DB pour les noms -->
+        <div class="menu_ressouces">
+            <a class="menu" href="?sectionId=<?= $section -> getMwIdSect() ?>"><?= $section -> getMwTitleSect() ;?></a>
+        <!--       <menu id="ressource-select">
+                <?php foreach($articleBySection as $abs):?>
+                        <li> <a href="#<?=$abs->getmwTitleArt()?>">
+                            <?=$abs->getmwTitleArt()?></a>
+                        </li>
+                <?php endforeach; ?>
+            </menu>  -->
+        </div>
+        <?php
+        endforeach;
+        ?> 
+        <div class="menu_ressouces">
+            <a href="?p=ressources">Ressources</a>
+            <!----sous-menu ressources   ----->
+            <menu id="ressource-select">     
+                <?php foreach($allCategory as $category):?>
+                    <li> <a href="#<?=$category->getMwTitleCategory()?>">
+                        <?=$category->getMwTitleCategory()?></a>
+                    </li>
+                <?php endforeach; ?>
+            </menu>
+        </div>
+        <a href="?p=agenda">Agenda</a>
+        <a href="?p=contact">Contact</a>
+        <a href="?p=livreDor">Livre D'or</a>
+    </nav>
+</div>
 
 <!-- Condition pour vérifier si l'utilisateur est connecté en tant qu'administrateur -->
