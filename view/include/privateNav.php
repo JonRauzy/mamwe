cb12bc631444e19a4ed09765cd4cefa7a0e4683f
JonRauzy <jonathan.rauzy@gmail.com>
2023-09-19 13:09:07 +0200

    quoi

--- view/include/privateNav.php
+++ view/include/privateNav.php
@@ -1,10 +1,12 @@
-<nav class="private-nav">
-    <a href="?p=admin">Accueil Admin</a>
-    <a href="?p=agendaCrud">Agenda</a>
-    <a href="?p=article">Article</a>
-    <a href="?p=info">Infos</a>
-    <a href="?p=livredorCrud">Livre d'or</a>
-    <a href="?p=ressourceCrud">Ressources</a>
-    <a href="?p=section">Section</a>
-    <a href="?p=user">Info perso</a> 
-</nav>
+<div class="private-nav">
+    <nav class="private-nav">
+        <a href="?p=admin"><i class="fa-solid fa-house"></i></a>
+        <a href="?p=agendaCrud">Agenda</a>
+        <a href="?p=article">Article</a>
+        <a href="?p=info">Infos</a>
+        <a href="?p=livredorCrud">Livre d'or</a>
+        <a href="?p=ressourceCrud">Ressources</a>
+        <a href="?p=section">Section</a>
+        <a href="?p=user">Info perso</a> 
+    </nav>
+</div>
