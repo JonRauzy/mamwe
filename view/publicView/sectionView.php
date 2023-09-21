632fc0d7cb04df38bd895f2d8255a65507b0f583
JonRauzy <jonathan.rauzy@gmail.com>
2023-09-21 09:24:40 +0200

    vue section

--- view/publicView/sectionView.php
+++ view/publicView/sectionView.php
@@ -16,48 +16,47 @@ include_once "../view/include/header.php";
 
 foreach ($articleBySection as $abs):  ?>
 
-<div><br>
-
+<div>
     <h2 class="sous-titre"><?= $abs -> getmwTitleArt();?></h2><br>
-    <p class="textsection"><?= $abs -> getmwContentArt();?></p>
+    <div class="text-section">
+        <?= $abs -> getmwContentArt();?>
+    </div>
     <div class="picture1">
     
    <?php
    
 $picture = $abs->getPicture();
 
-if ($picture !== null) {
-    $pic = explode("|||", $picture);
+    if ($picture !== null) {
+        $pic = explode("|||", $picture);
 
-    if (isset($pic[1])) {
-        $img1 = explode('---', $pic[1]);
-        echo '<img src="' . $img1[0] . '" alt="1" width="300px" ><br>';
+        if (isset($pic[1])) {
+            $img1 = explode('---', $pic[1]);
+            echo '<img src="' . $img1[0] . '" alt="1" width="300px" ><br>';
+        }
+
+        if (isset($pic[2])) {
+            $img2 = explode('---', $pic[2]);
+            echo '<img src="' . $img2[0] . '" alt="2" width="300px"  ><br>';
+        }
+
+        if (isset($pic[3])) {
+            $img3 = explode('---', $pic[3]);
+            echo '<img src="' . $img3[0] . '" alt="3" width="300px"  ><br>';
+        } 
     } else {
-        
+        echo 'Aucune image disponible<br>';
     }
+    ?>
+    </div>
 
-    if (isset($pic[2])) {
-        $img2 = explode('---', $pic[2]);
-        echo '<img src="' . $img2[0] . '" alt="2" width="300px"  ><br>';
-    } else {
-        
-    }
+    
+    <div class="article-date">
+        <p>Mis à jour le :</p>
+        <p><?= date('d/m/Y', strtotime($abs -> getmwDateArt())); ?></p>
+    </div>
+    
 
-    if (isset($pic[3])) {
-        $img3 = explode('---', $pic[3]);
-        echo '<img src="' . $img3[0] . '" alt="3" width="300px"  ><br>';
-    } else {
-        
-    }
-} else {
-    echo 'Aucune image disponible<br>';
-}
-?>
-
-</div>
-    <div class="empty"></div>
-    <p><?= $abs -> getmwDateArt(); ?></p>
-    <div class="empty"></div>
 </div>
 
 <?php endforeach ?>
