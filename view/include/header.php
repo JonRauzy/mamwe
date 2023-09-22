cec4956b22ee4bc31a633e6969dfa21509f94e6b
Jaouad1060 <jaouad1060@hotmail.com>
2023-09-22 12:24:04 +0200

    headerfr

--- view/include/header.php
+++ view/include/header.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="css/livredor.css">    
     <link rel="stylesheet" href="css/ressources.css">
     <link rel="stylesheet" href="css/crud.css">
+    <link rel="stylesheet" href="css/footer.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
